6a60fd1377a4a36dd145b4095e3bf79af13b3ae5
Gaurav Menghani <gaurav.menghani@gmail.com>
2017-06-11 10:10:09 -0700

    Running `yapf` on the frozen lake file

--- frozen-lake-iterative.py
+++ frozen-lake-iterative.py
@@ -34,16 +34,17 @@ while episode < NUM_EPISODES:
         prev_state = new_state
 
         if Q[prev_state].sum() > 0.0:
-            action = np.argmax(
-                Q[prev_state] + np.random.randn(1, env.action_space.n) * (NOISE / (episode + 1)))
+            action = np.argmax(Q[prev_state] + np.random.randn(
+                1, env.action_space.n) * (NOISE / (episode + 1)))
         else:
             action = np.random.choice(4)
 
         new_state, reward, done, _ = env.step(action)
 
         if episode == check:
-            print('Episode: {} Prev State was: {}, Action taken was: {}, next state was: {}, Path Len: {}'.format(
-                episode, prev_state, action, new_state, path_len))
+            print(
+                'Episode: {} Prev State was: {}, Action taken was: {}, next state was: {}, Path Len: {}'.
+                format(episode, prev_state, action, new_state, path_len))
 
         allow = False
         if allow or prev_state != new_state:
@@ -54,8 +55,10 @@ while episode < NUM_EPISODES:
             after_val = Q[prev_state][action]
 
             if episode == check:
-                print("Prev_State:{} Next_State:{} Prev_Val: {} Next_Max: {} After_Val: {}, Reward: {}".format(
-                    prev_state, new_state, prev_val, next_max, after_val, reward))
+                print(
+                    "Prev_State:{} Next_State:{} Prev_Val: {} Next_Max: {} After_Val: {}, Reward: {}".
+                    format(prev_state, new_state, prev_val, next_max,
+                           after_val, reward))
 
         if reward != 0:
             # print("----> Reward wasnt zero in Episode {} at state: {}, from state: {}, taking: {}  <----".format(episode, new_state, prev_state, action))
